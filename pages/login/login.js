f2205cd9e321c71ee569a0a18da2ecabf120b469
vcat <1991416497@qq.com>
2024-04-02 23:19:30 +0800

    complete login module

--- pages/login/login.js
+++ pages/login/login.js
@@ -1,38 +1,65 @@
 // pages/login/login.js
 Page({
   wxLogin() {
+    // 判断登录态 session_key 是否过期
+    wx.checkSession({
+      success: () => {
+        const third_session = wx.getStorageSync('third_session')
+        if (third_session) {
+          // third_session 存在，通过 third_session 登录；否则 通过 wx.login 登录
+          this.loginWithThirdSession(third_session);
+        } else {
+          this.wxLoginWithCode();
+        }
+      },
+      fail: () => {
+        this.wxLoginWithCode();
+      }
+    })
+  },
+  wxLoginWithCode() {
     wx.login({
       success: (res) => {
         if (res.code) {
           wx.request({
             url: 'http://localhost:8000/api/login',
-            data: {
-              code: res.code
-            },
+            data: { code: res.code },
             method: 'POST',
             timeout: 0,
             success: (result) => {
               console.log(result)
+              wx.setStorageSync('third_session', result.data.third_session)
             },
             fail: (err) => {
               console.log(err)
+              wx.showToast({ title: '一键登录失败，请重试', icon: 'none', duration: 2000 });
             },
           })
         } else {
-          wx.showToast({
-            title: '一键登录失败，请重试',
-            icon: 'none',
-            duration: 2000
-          });
+          wx.showToast({ title: '一键登录失败，请重试', icon: 'none', duration: 2000 });
         }
       },
       fail: (err) => {
-        wx.showToast({
-          title: '一键登录失败，请重试',
-          icon: 'none',
-          duration: 2000
-        });
+        console.log(err)
+        wx.showToast({ title: '一键登录失败，请重试', icon: 'none', duration: 2000 });
       }
     })
   },
+  loginWithThirdSession(third_session) {
+    wx.request({
+      url: 'http://localhost:8000/api/login',
+      data: { third_session: third_session },
+      method: 'POST',
+      timeout: 0,
+      success: (result) => {
+        console.log(result)
+      },
+      fail: (err) => {
+        console.log(err)
+        if (err.statusCode == 404) {
+          this.wxLoginWithCode();
+        }
+      },
+    })
+  }
 })
