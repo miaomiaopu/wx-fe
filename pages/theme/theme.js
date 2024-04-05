615322d02bcf7806ebac996ba9c2c699643d3bb7
vcat <1991416497@qq.com>
2024-04-05 14:27:27 +0800

    get user info module complete

--- pages/theme/theme.js
+++ pages/theme/theme.js
@@ -1,66 +1,41 @@
 // pages/theme/theme.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    nickname: "小小只",
+    avatar:
+      "https://thirdwx.qlogo.cn/mmopen/vi_32/POgEwh4mIHO4nibH0KlMECNjjGxQUq24ZEaGT4poC6icRiccVGKSyXwibcPq4BWmiaIGuG1icwxaQX6grC9VemZoJ8rg/132",
+    info_dot: false,
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad(options) {
-
+    this.getInfo();
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
-
+  getInfo: function () {
+    const third_session = wx.getStorageSync("third_session");
+    wx.request({
+      url: "http://localhost:8000/api/getInfo",
+      data: {
+        third_session: third_session,
+      },
+      method: "GET",
+      timeout: 0,
+      success: (result) => {
+        console.log(result);
+        if (result.statusCode == 404) {
+          wx.reLaunch({
+            url: "/pages/login/login",
+          });
+        } else if (result.statusCode == 200) {
+          this.setData({
+            nickname: result.data.nickname,
+            info_dot: result.data.info_dot,
+          });
+        }
+      },
+      fail: (err) => {
+        console.log(err);
+      },
+    });
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-
-  }
-})
+  infoChange: function () {},
+  commentInfo: function () {},
+});
