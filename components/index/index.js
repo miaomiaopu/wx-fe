9c19c8647e6b37516bc388338727b4596700a1cf
vcat <1991416497@qq.com>
2024-04-14 01:35:08 +0800

    get themes

--- components/index/index.js
+++ components/index/index.js
@@ -1,10 +1,30 @@
 // components/index/index.js
 Component({
   properties: {},
-  data: {},
+  data: {
+    my_themes: [
+      {
+        theme_id: 1,
+        theme_name: "test432424222222222222222222222222222222222222",
+        theme_picture: "http://localhost:8000/images/default-image.jpg",
+        tags: ["1", "2"],
+        total_subscription: 0,
+      },
+    ],
+    sub_themes: [
+      {
+        theme_id: 1,
+        theme_name: "test2",
+        theme_picture: "http://localhost:8000/images/default-image.jpg",
+        tags: ["1", "2", "12312312"],
+        total_subscription: 0,
+      },
+    ],
+  },
   lifetimes: {
     attached() {
       this.checkin();
+      this.getTheme();
     },
   },
   methods: {
@@ -27,7 +47,7 @@ Component({
           } else if (result.statusCode == 201) {
             wx.showToast({
               title: "^^ 今日签到~",
-              icon: "none"
+              icon: "none",
             });
           }
         },
@@ -38,8 +58,53 @@ Component({
     },
     toCreateTheme: function () {
       wx.navigateTo({
-        url: '../../pages/ctheme/ctheme',
-      })
-    }
+        url: "../../pages/ctheme/ctheme",
+      });
+    },
+    onMyThemeClick: function (event) {
+      const position = event.detail;
+      if (position === "left") {
+        console.log("update");
+      } else if (position === "cell") {
+        console.log("detail");
+      } else if (position === "right") {
+        console.log("delete");
+      }
+    },
+    onSubThemeClick: function (event) {
+      const position = event.detail;
+      if (position === "cell") {
+        console.log("detail");
+      } else if (position === "right") {
+        console.log("cancel");
+      }
+    },
+    getTheme: function () {
+      const third_session = wx.getStorageSync("third_session");
+      wx.request({
+        url: "http://localhost:8000/api/getMyThemeAndSubTheme",
+        data: {
+          third_session: third_session,
+        },
+        method: "GET",
+        timeout: 0,
+        success: (result) => {
+          console.log(result);
+          if (result.statusCode == 404) {
+            wx.reLaunch({
+              url: "/pages/login/login",
+            });
+          } else if (result.statusCode == 200) {
+            this.setData({
+              my_themes: result.data.my_themes,
+              sub_themes: result.data.sub_themes,
+            });
+          }
+        },
+        fail: (err) => {
+          console.log(err);
+        },
+      });
+    },
   },
 });
