c23977f8a90e62b0ec9575c2d1ba49b81374a99d
vcat <1991416497@qq.com>
2024-04-15 12:04:58 +0800

    delete my theme and unsub theme

--- components/index/index.js
+++ components/index/index.js
@@ -63,20 +63,85 @@ Component({
     },
     onMyThemeClick: function (event) {
       const position = event.detail;
+      const index = event.currentTarget.dataset.index;
+      const third_session = wx.getStorageSync("third_session");
+
       if (position === "left") {
         console.log("update");
       } else if (position === "cell") {
         console.log("detail");
       } else if (position === "right") {
-        console.log("delete");
+        wx.request({
+          url: "http://localhost:8000/api/deleteMyTheme",
+          data: {
+            third_session: third_session,
+            theme_id: this.data.my_themes[index].theme_id,
+          },
+          method: "POST",
+          timeout: 0,
+          success: (result) => {
+            console.log(result);
+            if (result.statusCode == 404) {
+              wx.reLaunch({
+                url: "/pages/login/login",
+              });
+            } else if (result.statusCode == 200) {
+              this.data.my_themes.splice(index, 1);
+              this.setData({
+                my_themes: this.data.my_themes,
+              });
+            } else {
+              wx.showToast({
+                title: "删除失败",
+                icon: "error",
+              });
+            }
+          },
+          fail: (err) => {
+            console.log(err);
+          },
+        });
       }
     },
     onSubThemeClick: function (event) {
       const position = event.detail;
+      const index = event.currentTarget.dataset.index;
       if (position === "cell") {
         console.log("detail");
       } else if (position === "right") {
-        console.log("cancel");
+        const third_session = wx.getStorageSync("third_session");
+
+        // 取消订阅
+        wx.request({
+          url: "http://localhost:8000/api/cancelSubTheme",
+          data: {
+            third_session: third_session,
+            theme_id: this.data.sub_themes[index].theme_id,
+          },
+          method: "POST",
+          timeout: 0,
+          success: (result) => {
+            console.log(result);
+            if (result.statusCode == 404) {
+              wx.reLaunch({
+                url: "/pages/login/login",
+              });
+            } else if (result.statusCode == 200) {
+              this.data.sub_themes.splice(index, 1);
+              this.setData({
+                sub_themes: this.data.sub_themes,
+              });
+            } else {
+              wx.showToast({
+                title: "取消失败",
+                icon: "error",
+              });
+            }
+          },
+          fail: (err) => {
+            console.log(err);
+          },
+        });
       }
     },
     getTheme: function () {
