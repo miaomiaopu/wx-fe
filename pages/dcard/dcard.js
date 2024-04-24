f5b792dc5693e4c17f868b645e7b265d6951c686
vcat <1991416497@qq.com>
2024-04-24 20:23:39 +0800

    like and unlike

--- pages/dcard/dcard.js
+++ pages/dcard/dcard.js
@@ -10,15 +10,44 @@ Page({
     image1: "",
     image2: "",
     image3: "",
+    is_like: true,
   },
   onLoad(options) {
     const card_id_param = parseInt(options.card_id);
-    const card_title = options.card_title
+    const card_title = options.card_title;
     this.setData({
       card_id: card_id_param,
-      card_title: card_title
-    })
+      card_title: card_title,
+    });
+    this.getLike(card_id_param);
     this.getCard(card_id_param);
+  },
+  getLike(card_id) {
+    const third_session = wx.getStorageSync("third_session");
+    wx.request({
+      url: "http://localhost:8000/api/isLike",
+      data: {
+        third_session: third_session,
+        card_id: card_id,
+      },
+      method: "GET",
+      timeout: 0,
+      success: (result) => {
+        console.log(result);
+        if (result.statusCode == 404) {
+          wx.reLaunch({
+            url: "/pages/login/login",
+          });
+        } else if (result.statusCode == 200) {
+          this.setData({
+            is_like: result.data.is_like,
+          });
+        }
+      },
+      fail: (err) => {
+        console.log(err);
+      },
+    });
   },
   getCard(card_id) {
     const third_session = wx.getStorageSync("third_session");
@@ -45,6 +74,64 @@ Page({
             image1: result.data.image1,
             image2: result.data.image2,
             image3: result.data.image3,
+          });
+        }
+      },
+      fail: (err) => {
+        console.log(err);
+      },
+    });
+  },
+  like() {
+    const third_session = wx.getStorageSync("third_session");
+    const card_id = this.data.card_id;
+
+    wx.request({
+      url: "http://localhost:8000/api/likeCard",
+      data: {
+        third_session: third_session,
+        card_id: card_id,
+      },
+      method: "POST",
+      timeout: 0,
+      success: (result) => {
+        console.log(result);
+        if (result.statusCode == 404) {
+          wx.reLaunch({
+            url: "/pages/login/login",
+          });
+        } else if (result.statusCode == 201) {
+          this.setData({
+            is_like: true,
+          });
+        }
+      },
+      fail: (err) => {
+        console.log(err);
+      },
+    });
+  },
+  unlike() {
+    const third_session = wx.getStorageSync("third_session");
+    const card_id = this.data.card_id;
+
+    wx.request({
+      url: "http://localhost:8000/api/unlikeCard",
+      data: {
+        third_session: third_session,
+        card_id: card_id,
+      },
+      method: "POST",
+      timeout: 0,
+      success: (result) => {
+        console.log(result);
+        if (result.statusCode == 404) {
+          wx.reLaunch({
+            url: "/pages/login/login",
+          });
+        } else if (result.statusCode == 200) {
+          this.setData({
+            is_like: false,
           });
         }
       },
